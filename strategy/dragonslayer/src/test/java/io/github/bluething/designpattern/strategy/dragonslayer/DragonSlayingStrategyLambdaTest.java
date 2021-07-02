b3443e63d14f91d7a4c3fea8f7c03ca7511fa9a5
Habib Ali Machpud <habib.machpud@pegipegi.com>
2021-07-02 17:43:45 +0700

    Using ParameterizedTest to test enum

--- strategy/dragonslayer/src/test/java/io/github/bluething/designpattern/strategy/dragonslayer/DragonSlayingStrategyLambdaTest.java
+++ strategy/dragonslayer/src/test/java/io/github/bluething/designpattern/strategy/dragonslayer/DragonSlayingStrategyLambdaTest.java
@@ -4,13 +4,35 @@ import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.AppenderBase;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.slf4j.LoggerFactory;
 
+import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
 public class DragonSlayingStrategyLambdaTest {
+
+    static Collection<Object[]> dataProvider() {
+        return List.of(
+                new Object[] {
+                        DragonSlayingStrategyLambda.Strategy.MeleeStrategy,
+                        "Melee attack!"
+                },
+                new Object[] {
+                        DragonSlayingStrategyLambda.Strategy.ProjectileStrategy,
+                        "Projectile attack!"
+                },
+                new Object[]{
+                        DragonSlayingStrategyLambda.Strategy.SpellStartegy,
+                        "Spell attack!"
+                }
+        );
+    }
 
     private InMemmoryAppender appender;
 
@@ -21,6 +43,20 @@ public class DragonSlayingStrategyLambdaTest {
     @AfterEach
     public void tearDown() {
         appender.stop();
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void strategyLambdaExecuteUsingParameterizedDataWriteSameMessageInLog(DragonSlayingStrategy dragonSlayingStrategy, String expectedResult) {
+        dragonSlayingStrategy.execute();
+        Assertions.assertEquals(expectedResult, appender.getLastMessage());
+    }
+
+    @ParameterizedTest
+    @MethodSource("dataProvider")
+    public void strategyExecuteLambdaUsingParameterizedDataResultingLogWithSizeOneForOneCall(DragonSlayingStrategy dragonSlayingStrategy, String expectedResult) {
+        dragonSlayingStrategy.execute();
+        Assertions.assertEquals(1, appender.getLogSize());
     }
 
     class InMemmoryAppender extends AppenderBase<ILoggingEvent> {
