e76402c298a4a6cf7c8d470fc7fdb169acb3b2ce
Habib Ali Machpud <habib.machpud@pegipegi.com>
2021-07-02 17:35:50 +0700

    Custom appender

---
+++ strategy/dragonslayer/src/test/java/io/github/bluething/designpattern/strategy/dragonslayer/DragonSlayingStrategyLambdaTest.java
@@ -0,0 +1,49 @@
+package io.github.bluething.designpattern.strategy.dragonslayer;
+
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.AppenderBase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.slf4j.LoggerFactory;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class DragonSlayingStrategyLambdaTest {
+
+    private InMemmoryAppender appender;
+
+    @BeforeEach
+    public void setup() {
+        appender = new InMemmoryAppender();
+    }
+    @AfterEach
+    public void tearDown() {
+        appender.stop();
+    }
+
+    class InMemmoryAppender extends AppenderBase<ILoggingEvent> {
+
+        private final List<ILoggingEvent> log;
+
+        public InMemmoryAppender() {
+            log = new LinkedList<>();
+            ((Logger) LoggerFactory.getLogger("root")).addAppender(this);
+            start();
+        }
+
+        @Override
+        protected void append(ILoggingEvent eventObject) {
+            log.add(eventObject);
+        }
+
+        public int getLogSize() {
+            return log.size();
+        }
+
+        public String getLastMessage() {
+            return log.get(log.size() - 1).getFormattedMessage();
+        }
+    }
+}
